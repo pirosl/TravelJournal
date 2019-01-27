d44169b2ec224df49c980f708b12642db589d0de
Lucian Piros <pirosl@yahoo.com>
2019-01-27 00:01:05 +0000

    Add Note model class

---
+++ app/src/main/java/com/lucianpiros/traveljournal/model/Note.java
@@ -0,0 +1,26 @@
+package com.lucianpiros.traveljournal.model;
+
+public class Note {
+    private String noteTitle;
+    private String noteContent;
+
+
+    public Note() {
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
