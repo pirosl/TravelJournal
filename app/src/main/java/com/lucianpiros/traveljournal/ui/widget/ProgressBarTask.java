92e237cb9c9d6615680d81fad2a488a2c409b951
Lucian Piros <pirosl@yahoo.com>
2019-02-07 14:11:40 +0000

    Move ProgressBar in a separate widget class

---
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/widget/ProgressBarTask.java
@@ -0,0 +1,53 @@
+package com.lucianpiros.traveljournal.ui.widget;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+
+import java.util.concurrent.TimeUnit;
+
+public class ProgressBarTask extends AsyncTask<Void, Void, Void> {
+
+    private ViewGroup progressBarHolder;
+    private Activity activity;
+
+    public ProgressBarTask(ViewGroup progressBarHolder, Activity activity) {
+        this.progressBarHolder = progressBarHolder;
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressBarHolder.setVisibility(View.VISIBLE);
+        AlphaAnimation inAnimation = new AlphaAnimation(0f, 1f);
+        inAnimation.setDuration(200);
+        progressBarHolder.setAnimation(inAnimation);
+        progressBarHolder.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        progressBarHolder.setVisibility(View.GONE);
+        AlphaAnimation outAnimation = new AlphaAnimation(1f, 0f);
+        outAnimation.setDuration(200);
+        progressBarHolder.setAnimation(outAnimation);
+        progressBarHolder.setVisibility(View.GONE);
+        activity.finish();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            while (true) {
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
