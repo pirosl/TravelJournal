b4893aef19fb4c62ca771e7643c027b18b498f43
Lucian Piros <pirosl@yahoo.com>
2019-02-11 18:37:08 +0000

    Add javadoc documentation and format codeAdd javadoc documentation and format code

--- app/src/main/java/com/lucianpiros/traveljournal/ui/widget/CustomAlertDialog.java
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/widget/CustomAlertDialog.java
@@ -16,17 +16,48 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * This class builds and manages a custom alert dialog used when selecting photo or videos.
+ * Alert dialog title has a custom color.
+ * Alert dialog body contains 3 buttons with custom background and tint color. Each button
+ * displays an option on where phot/video can be selected from.
+ *
+ * @author Lucian Piros
+ * @version 1.0
+ */
 public class CustomAlertDialog {
+
+    /**
+     * Methids from this interface are called when buttons are tapped / pressed
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     public interface CustomDialogActionListener {
         void onOption1(int dialogType);
+
         void onOption2(int dialogType);
     }
 
+    /**
+     * Inner class holding alert dialog title widgets.
+     * Used in conjunction with ButterKnife
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     class Title {
         @BindView(R.id.alertdialog_title)
         TextView valueTV;
     }
 
+    /**
+     * Inner class holding alert dialog body widgets.
+     * Used in conjunction with ButterKnife
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     class Body {
         @BindView(R.id.button_option1)
         Button option1BT;
@@ -53,18 +84,34 @@ public class CustomAlertDialog {
         }
     }
 
+    // Dialog type. Can be PHOTO or MOVIE
     private int dialogType;
+    // Layout inflater - needed to inflate views within Dialog
     private LayoutInflater layoutInflater;
     private Context context;
     private Dialog alertDialog;
     private CustomDialogActionListener customDialogActionListener;
 
+    /**
+     * @param dialogType     - dialog type (eg. PHOTO or MOVIE)
+     * @param layoutInflater - layout inflater used to inflate Dialog inner views
+     * @param context        - Context used within
+     */
     public CustomAlertDialog(int dialogType, LayoutInflater layoutInflater, Context context) {
         this.dialogType = dialogType;
         this.layoutInflater = layoutInflater;
         this.context = context;
     }
 
+    /**
+     * Initialise this alert dialog
+     *
+     * @param viewGroup    view group used when inflating body view
+     * @param titleResid   String used as title for this alert dialog
+     * @param option1Resid String used for option1 button
+     * @param option2Resid String used for option1 button
+     * @param option3Resid String used for option1 button
+     */
     public void initialize(@NotNull ViewGroup viewGroup, int titleResid, int option1Resid, int option2Resid, int option3Resid) {
         final CustomAlertDialog.Title alertDialogTitle = new Title();
         final CustomAlertDialog.Body alertDialogBody = new Body();
@@ -89,6 +136,9 @@ public class CustomAlertDialog {
         this.customDialogActionListener = customDialogActionListener;
     }
 
+    /**
+     * Show custom built alert dialog
+     */
     public void show() {
         alertDialog.show();
     }
