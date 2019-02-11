e3b457f60a698d44c3019f9c395c86bc0f4750fa
Lucian Piros <pirosl@yahoo.com>
2019-02-11 18:29:39 +0000

    Add javadoc documentation and format code

--- app/src/main/java/com/lucianpiros/traveljournal/ui/widget/ConfirmationDialog.java
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/widget/ConfirmationDialog.java
@@ -17,18 +17,47 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * This class builds and manages a custom alert dialog.
+ * Alert dialog title has a custom color.
+ * Alert dialog body contains 2 buttons with custom background and tint color
+ *
+ * @author Lucian Piros
+ * @version 1.0
+ */
 public class ConfirmationDialog {
 
+    /**
+     * Methids from this interface are called when buttons are tapped / pressed
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     public interface ConfirmationDialogActionListener {
         void onAccept();
+
         void onDecline();
     }
 
+    /**
+     * Inner class holding alert dialog title widgets.
+     * Used in conjunction with ButterKnife
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     class Title {
         @BindView(R.id.alertdialog_title)
         TextView valueTV;
     }
 
+    /**
+     * Inner class holding alert dialog body widgets.
+     * Used in conjunction with ButterKnife
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
     class Body {
         @BindView(R.id.button_accept)
         ImageButton option1BT;
@@ -48,18 +77,32 @@ public class ConfirmationDialog {
         }
     }
 
+    // ConfirnationDialogAction listener
     private ConfirmationDialogActionListener confirmationDialogActionListener;
 
+    // Layout inflater - needed to inflate views within Dialog
     private LayoutInflater layoutInflater;
     private Context context;
     private Dialog alertDialog;
 
+    /**
+     * Class constructor
+     *
+     * @param layoutInflater - layout inflater used to inflate Dialog inner views
+     * @param context        - Context used within
+     */
     public ConfirmationDialog(LayoutInflater layoutInflater, Context context) {
 
         this.layoutInflater = layoutInflater;
         this.context = context;
     }
 
+    /**
+     * Initialize this alert dialog
+     *
+     * @param viewGroup   view group used when inflating body view
+     * @param titleString String used as title for this alert dialog
+     */
     public void initialize(@NotNull ViewGroup viewGroup, String titleString) {
         final Title alertDialogTitle = new Title();
         final Body alertDialogBody = new Body();
@@ -81,6 +124,9 @@ public class ConfirmationDialog {
         this.confirmationDialogActionListener = confirmationDialogActionListener;
     }
 
+    /**
+     * Show custom built alert dialog
+     */
     public void show() {
         alertDialog.show();
     }
