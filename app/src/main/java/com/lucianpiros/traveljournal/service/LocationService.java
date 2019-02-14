e755c6880f5e63630420cfc10919479b0f1d642c
Lucian Piros <pirosl@yahoo.com>
2019-02-14 11:28:46 +0000

    Fix permission check error

--- app/src/main/java/com/lucianpiros/traveljournal/service/LocationService.java
+++ app/src/main/java/com/lucianpiros/traveljournal/service/LocationService.java
@@ -63,11 +63,9 @@ public class LocationService implements LocationListener {
             locationService.locationManager = (LocationManager) (activity.getSystemService(Context.LOCATION_SERVICE));
             if (activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && activity.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                         0);
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        0);
+                return;
             }
             locationService.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationService);
             initialized = true;
