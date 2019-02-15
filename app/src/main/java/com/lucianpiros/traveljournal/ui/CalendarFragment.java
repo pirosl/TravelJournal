497ade3f872dcdbac4d4769afa182628f6119db7
Lucian Piros <pirosl@yahoo.com>
2019-02-15 16:13:29 +0000

    Add async task - fill calendar with info on when notes were added

--- app/src/main/java/com/lucianpiros/traveljournal/ui/CalendarFragment.java
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/CalendarFragment.java
@@ -1,16 +1,22 @@
 package com.lucianpiros.traveljournal.ui;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.lucianpiros.traveljournal.R;
+import com.lucianpiros.traveljournal.data.DataCache;
+import com.lucianpiros.traveljournal.model.Note;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -19,12 +25,17 @@ import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Calendar fragment. Displays a calendar, each date will have a note icon under it if there is any note
+ * created within that particular day.
+ *
+ * @author Lucian Piros
+ * @version 1.0
+ */
 public class CalendarFragment extends Fragment {
 
     @BindView(R.id.calendar)
     CalendarView calendarCV;
-
-    private List<EventDay> eventDays = new ArrayList<>();
 
     public CalendarFragment() {
     }
@@ -40,27 +51,55 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //this.layoutInflater = inflater;
-        //this.viewGroup = container;
-
         View calendarView = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         ButterKnife.bind(this, calendarView);
 
-        Calendar calendar = new GregorianCalendar(2019,01,21);
-        //calendarCV.setDate(calendar);
-        EventDay eventDay = new EventDay(calendar, R.drawable.ic_notes);
-        eventDays.add(eventDay);
-        calendarCV.setEvents(eventDays);
+        new SetupCalendarTask().execute("");
 
-        // Retrieve recipe idx passed as parameter from Main Activity
-        /*Bundle bundle = getArguments();
-
-        noteIdx = NOTSELECTED_IDX;
-        if(bundle != null)
-            noteIdx = bundle.getInt(getResources()
-                    .getString(R.string.noteactivity_extra_param));
-*/
         return calendarView;
+    }
+
+    /**
+     * AsyncTask class used to fill the notes icon under dates within which notes were created
+     *
+     * @author Lucian Piros
+     * @version 1.0
+     */
+    private class SetupCalendarTask extends AsyncTask<String, Void, String> {
+
+        private List<EventDay> eventDays;
+
+        protected SetupCalendarTask() {
+            eventDays = new ArrayList<>();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            List<Note> notes = DataCache.getInstance().getNotesList();
+            if(notes != null) {
+                for (Note note : notes) {
+                    Date creationDate = note.getNoteCreationDate();
+                    Calendar calendar = new GregorianCalendar(1900 + creationDate.getYear(), creationDate.getMonth(), creationDate.getDay());
+                    EventDay eventDay = new EventDay(calendar, R.drawable.ic_notes);
+
+                    eventDays.add(eventDay);
+                }
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // set events days
+            calendarCV.setEvents(eventDays);
+            calendarCV.setOnDayClickListener(new OnDayClickListener() {
+                @Override
+                public void onDayClick(EventDay eventDay) {
+                    Calendar c = eventDay.getCalendar();
+                    Log.d("Test", "clicked on " + c.toString());
+                }
+            });
+        }
     }
 }
