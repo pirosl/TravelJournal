5703716ba70c2af8e6825aa4ecd3183a04fec8e7
Lucian Piros <pirosl@yahoo.com>
2019-02-16 23:47:00 +0000

    Fix option menu titles

--- app/src/main/java/com/lucianpiros/traveljournal/ui/MapContainerFragment.java
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/MapContainerFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import com.lucianpiros.traveljournal.R;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -18,10 +19,18 @@ import androidx.fragment.app.Fragment;
 public class MapContainerFragment extends Fragment {
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         /* Inflate the layout for this fragment */
         View view = inflater.inflate(R.layout.fragment_mapcontainer, container, false);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
 
         getFragmentManager().beginTransaction().replace(R.id.mapcontainer, new MapFragment()).commit();
 
