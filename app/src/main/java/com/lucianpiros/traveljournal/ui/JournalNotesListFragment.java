60c96ddbc93de4e5c8185d4b6a957d677836ea51
Lucian Piros <pirosl@yahoo.com>
2019-02-16 18:45:26 +0000

    Change activity title for filtered list

--- app/src/main/java/com/lucianpiros/traveljournal/ui/JournalNotesListFragment.java
+++ app/src/main/java/com/lucianpiros/traveljournal/ui/JournalNotesListFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.core.os.ConfigurationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -26,6 +27,7 @@ import com.lucianpiros.traveljournal.data.adapter.AdapterFilter;
 import com.lucianpiros.traveljournal.data.adapter.NotesAdapter;
 import com.lucianpiros.traveljournal.model.Note;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
@@ -91,6 +93,10 @@ public class JournalNotesListFragment extends Fragment implements FirebaseDB.Not
                 calendar.setTimeZone(TimeZone.getTimeZone(timezone));
                 calendar.setTimeInMillis(millis);
                 adapterFilter.setCalendar(calendar);
+
+                SimpleDateFormat dateSF = new SimpleDateFormat(getString(R.string.note_dateformat), ConfigurationCompat.getLocales(getResources().getConfiguration()).get(0));
+                String message = String.format(getResources().getString(R.string.noteslistfiltered_ondate), dateSF.format(calendar.getTime()));
+                getActivity().setTitle(message);
             }
         }
 
